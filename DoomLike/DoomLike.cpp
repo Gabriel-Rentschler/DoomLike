2af7f8193cc5a8541bf0542a9793da4ff394921d
Gabriel Araujo <gabrielrdearaujo1023@hotmail.com>
2023-10-21 19:39:34 -0300

    desenhar a tela "3D" ao lado do mapa, retirar o preenchimento do espaço com um gradiente e deixar a cor branca no lugar

--- DoomLike/DoomLike.cpp
+++ DoomLike/DoomLike.cpp
@@ -24,7 +24,7 @@ void draw_rectangle(std::vector<uint32_t>& img, const size_t img_w, const size_t
         for (size_t j = 0; j < h; j++) {
             size_t cx = x + i;
             size_t cy = y + j;
-            assert(cx < img_w&& cy < img_h);
+            if (cx >= img_w || cy >= img_h) continue;
             img[cx + cy * img_w] = color;
         }
     }
@@ -44,9 +44,9 @@ void drop_ppm_image(const std::string filename, const std::vector<uint32_t>& ima
 
 int main()
 {
-    const size_t win_w = 512; // image width
+    const size_t win_w = 1024; // image width
     const size_t win_h = 512; // image height
-    std::vector<uint32_t> framebuffer(win_w * win_h, 255); // the image itself, initialized to red
+    std::vector<uint32_t> framebuffer(win_w * win_h, pack_color(255,255,255)); // the image itself, initialized to red
 
     const size_t map_w = 16;
     const size_t map_h = 16;
@@ -75,16 +75,9 @@ int main()
 
     assert(sizeof(map) == map_w * map_h + 1); // +1 for the null terminated string
 
-    for (size_t j = 0; j < win_h; j++) { // fill the screen with color gradients
-        for (size_t i = 0; i < win_w; i++) {
-            uint8_t r = 255 * j / float(win_h); // varies between 0 and 255 as j sweeps the vertical
-            uint8_t g = 255 * i / float(win_w); // varies between 0 and 255 as i sweeps the horizontal
-            uint8_t b = 0;
-            framebuffer[i + j * win_w] = pack_color(r, g, b);
-        }
-    }
+    
 
-    const size_t rect_w = win_w / map_w;
+    const size_t rect_w = win_w / (map_w * 2);
     const size_t rect_h = win_h / map_h;
     for (size_t j = 0; j < map_h; j++) { // draw the map
         for (size_t i = 0; i < map_w; i++) {
@@ -96,21 +89,27 @@ int main()
     }
 
     //Draw player on the map
-    draw_rectangle(framebuffer, win_w, win_h, player_x*rect_w, player_y*rect_h, 4, 4, pack_color(255, 255, 255));
+    draw_rectangle(framebuffer, win_w, win_h, player_x*rect_w, player_y*rect_h, 4, 4, pack_color(0, 0, 0));
 
-    //Draw the fov cone
-    for (float i=0; i < win_w; i++) { 
-        float angle = player_a - fov / 2 + fov * i / float(win_w);
+    //Draw the fov cone and the 3D view
+    for (float i=0; i < win_w / 2; i++) { 
+        float angle = player_a - fov / 2 + fov * i / float(win_w / 2);
 
         for (float c = 0; c < 20; c += .05) {
             float cx = player_x + c * cos(angle);
             float cy = player_y + c * sin(angle);
-            if (map[int(cx) + int(cy) * map_w] != ' ') break;
 
             size_t pix_x = cx * rect_w;
             size_t pix_y = cy * rect_h;
 
-            framebuffer[pix_x + pix_y * win_w] = pack_color(255, 255, 255);
+            framebuffer[pix_x + pix_y * win_w] = pack_color(160, 160, 160);
+
+            if (map[int(cx) + int(cy) * map_w] != ' ') { //happens when the ray touches a wall, drawing the vertical column to create the "3D"
+                size_t column_height = win_h / c;
+                draw_rectangle(framebuffer, win_w, win_h, win_w / 2 + i, win_h / 2 - column_height / 2, 1, column_height, pack_color(0, 255, 255));
+
+                break;
+            }
         }
     }
 
